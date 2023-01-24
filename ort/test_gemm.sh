16d7f2023680e499c6ab607a61bdcfad42f4449e
PeixuanZuo <94887879+PeixuanZuo@users.noreply.github.com>
2023-01-25 00:45:37 +0800

    Update test_gemm.sh

--- ort/test_gemm.sh
+++ ort/test_gemm.sh
@@ -3,7 +3,7 @@
 THIS_DIR=$(realpath $(dirname $0))
 export KERNEL_EXPLORER_BUILD_DIR="${THIS_DIR}/onnxruntime/build/Release/"
 
-python ${THIS_DIR}/onnxruntime/onnxruntime/python/tools/kernel_explorer/kernels/gemm_test.py N N float16 49152 3072 768
+python ${THIS_DIR}/onnxruntime/onnxruntime/python/tools/kernel_explorer/kernels/gemm_test.py float16 N N  49152 3072 768
 
 #change the init method
 # ${onnxruntime-src}/onnxruntime/python/tools/kernel_explorer/kernels/gemm_test.py 119~120
